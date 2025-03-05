07ac19b01ee1cdcf903c22485d73e53817538f5f
ZSedillo <sedillozandro720@gmail.com>
2025-03-06 00:01:33 +0800

    Fixing education and experience design

--- src/Education/Education.jsx
+++ src/Education/Education.jsx
@@ -1,33 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
-
-function Education(){
-    const title = {
-        fontFamily: 'Inter, sans-serif',
-        fontWeight: 600,
-        fontSize: '62.5px',
-        marginTop: '100px',
-        marginBottom: '25px',
-        textAlign: 'center',
-    };
-
-    const box = {
-        backgroundColor: '#001011',
-        color: '#ffff',
-        display: 'flex',
-        width: '60px',
-        justifyContent: 'center',
-        marginLeft:'20px',
-        marginRight:'20px',
-    };
-
-    const year = {
-        color: '#ffff',
-        fontSize: '15px',
-        lineHeight: '37px',
-        zIndex:'2',
-    };
-
+function Education() {
     useEffect(() => {
         const handleScroll = () => {
             document.querySelectorAll(".section-load-left").forEach(dataLoad => {
@@ -48,40 +21,59 @@ function Education(){
         };
 
         window.addEventListener("scroll", handleScroll);
-        handleScroll(); // Initial check in case the elements are already in view
+        handleScroll();
 
         return () => {
             window.removeEventListener("scroll", handleScroll);
         };
-    }, []);       
+    }, []);
 
     return (
-        <>
-            <div>
-                <h1 className="section-load-left" style={title}>Education</h1>
-            </div>
-            <div style={{ backgroundColor: '#FFFF', padding: '100px 0' ,boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'}}>
-                <div className="experience-item section-load-left">
-                    <div className='shape-container'>
-                        <div style={box}><h5 style={year}>Present</h5></div>
-                        {/* <div className='triangle'></div>
-                        <div className='circle'></div>
-                        <div className='vertical-line'></div> */}
-                    </div>
-                    <div className="experience-content">
-                        <h2>University of Santo Thomas</h2>
-                        <h3>Computer Science Student</h3>
-                        <br/>
-                        <ul>
-                            <li>Learned basics of HTML and CSS</li>
-                            <li>Learned Java, basics of PHP and Xampp, MySQL</li>
-                            <li>Worked on website projects using HTML, CSS, Java, Derby, and SQL with two databases connected and executed SQL in NetBeans</li>
-                        </ul>
-                    </div>
+        <div style={{ padding: '100px 0', backgroundColor: '#fff', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+            <h1 className="section-load-left" style={{
+                fontFamily: 'Arial, sans-serif',
+                fontWeight: 700,
+                fontSize: '50px',
+                marginTop: '100px',
+                marginBottom: '25px',
+                textAlign: 'center',
+                color: '#001011'
+            }}>Education</h1>
+
+            <div className="experience-item section-load-left" style={{
+                display: 'flex',
+                alignItems: 'center',
+                padding: '20px',
+                borderRadius: '10px',
+                backgroundColor: '#f9f9f9',
+                boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+                margin: '20px auto',
+                maxWidth: '800px'
+            }}>
+                <div style={{
+                    backgroundColor: '#001011',
+                    color: '#fff',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '80px',
+                    height: '40px',
+                    borderRadius: '5px',
+                    fontSize: '15px',
+                    fontWeight: 'bold'
+                }}>Present</div>
+                <div className="experience-content" style={{ marginLeft: '20px' }}>
+                    <h2 style={{ margin: 0, fontSize: '24px', fontWeight: 'bold' }}>University of Santo Tomas</h2>
+                    <h3 style={{ margin: '5px 0', fontSize: '18px', color: '#747274' }}>Computer Science Student</h3>
+                    <ul style={{ padding: 0, marginTop: '10px', listStyleType: 'none' }}>
+                        <li style={{ marginBottom: '5px' }}>Learned basics of HTML and CSS</li>
+                        <li style={{ marginBottom: '5px' }}>Learned Java, basics of PHP and XAMPP, MySQL</li>
+                        <li>Worked on website projects using HTML, CSS, Java, Derby, and SQL with two databases connected</li>
+                    </ul>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
-export default Education
+export default Education;
--- src/Experience/Experience.jsx
+++ src/Experience/Experience.jsx
@@ -1,36 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import './Experience.css';
-import { height } from '@mui/system';
-import zIndex from '@mui/material/styles/zIndex';
+import React, { useEffect } from 'react';
 
 function Experience() {
-    const title = {
-        fontFamily: 'Inter, sans-serif',
-        fontWeight: 600,
-        fontSize: '62.5px',
-        marginTop: '100px',
-        marginBottom: '25px',
-        textAlign: 'center',
-    };
-
-    const box = {
-        backgroundColor: '#001011',
-        color: '#ffff',
-        display: 'flex',
-        width: '60px',
-        justifyContent: 'center',
-        marginLeft:'20px',
-        marginRight:'20px',
-    };
-
-    const year = {
-        color: '#ffff',
-        fontSize: '15px',
-        lineHeight: '37px',
-        zIndex:'2',
-    };
-
-
     useEffect(() => {
         const handleScroll = () => {
             document.querySelectorAll(".section-load-left").forEach(dataLoad => {
@@ -51,41 +21,60 @@ function Experience() {
         };
 
         window.addEventListener("scroll", handleScroll);
-        handleScroll(); // Initial check in case the elements are already in view
+        handleScroll();
 
         return () => {
             window.removeEventListener("scroll", handleScroll);
         };
-    }, []);      
+    }, []);
 
     return (
-        <>
-            <div>
-                <h1 style={title} className='section-load-left'>Experience</h1>
-            </div>
-            <div style={{ backgroundColor: '#FFFF', padding: '100px 0', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'}}>
-                <div className="experience-item section-load-left">
-                    <div className='shape-container'>
-                        <div style={box}><h5 style={year}>2024</h5></div>
-                        {/* <div className='triangle'></div>
-                        <div className='circle'></div>
-                        <div className='vertical-line'></div> */}
-                    </div>
-                    <div className="experience-content">
-                        <h2>Full Stack Developer Intern</h2>
-                        <h3>Fildev</h3>
-                        <br />
-                        <ul>
-                            <li>Developed and managed APIs, contributing to both front-end and back-end</li>
-                            <li>Assisted with UI/UX design and implemented features.</li>
-                            <li>Worked on server management using Putty and SSH.</li>
-                            <li>Utilized MongoDB Compass for database management.</li>
-                            <li>Gained proficiency in version control with GitHub.</li>
-                        </ul>
-                    </div>
+        <div style={{ padding: '100px 0', backgroundColor: '#fff', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+            <h1 className="section-load-left" style={{
+                fontFamily: 'Arial, sans-serif',
+                fontWeight: 700,
+                fontSize: '50px',
+                marginTop: '100px',
+                marginBottom: '25px',
+                textAlign: 'center',
+                color: '#001011'
+            }}>Experience</h1>
+
+            <div className="experience-item section-load-left" style={{
+                display: 'flex',
+                alignItems: 'center',
+                padding: '20px',
+                borderRadius: '10px',
+                backgroundColor: '#f9f9f9',
+                boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+                margin: '20px auto',
+                maxWidth: '800px'
+            }}>
+                <div style={{
+                    backgroundColor: '#001011',
+                    color: '#fff',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '80px',
+                    height: '40px',
+                    borderRadius: '5px',
+                    fontSize: '15px',
+                    fontWeight: 'bold'
+                }}>2024</div>
+                <div className="experience-content" style={{ marginLeft: '20px' }}>
+                    <h2 style={{ margin: 0, fontSize: '24px', fontWeight: 'bold' }}>Full Stack Developer Intern</h2>
+                    <h3 style={{ margin: '5px 0', fontSize: '18px', color: '#747274' }}>Fildev</h3>
+                    <ul style={{ padding: 0, marginTop: '10px', listStyleType: 'none' }}>
+                        <li style={{ marginBottom: '5px' }}>Developed and managed APIs, contributing to both front-end and back-end</li>
+                        <li style={{ marginBottom: '5px' }}>Assisted with UI/UX design and implemented features</li>
+                        <li style={{ marginBottom: '5px' }}>Worked on server management using Putty and SSH</li>
+                        <li style={{ marginBottom: '5px' }}>Utilized MongoDB Compass for database management</li>
+                        <li>Gained proficiency in version control with GitHub</li>
+                    </ul>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 

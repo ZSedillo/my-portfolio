d41b6a48ebc0623a9f5c56133f61c4bf4510cd5e
ZSedillo <sedillozandro720@gmail.com>
2025-08-08 10:25:18 +0800

    adding cv linking to about page

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Resume from '../assets/resume/Sedillo_CV.pdf';
 
 const About = () => {
   const [isVisible, setIsVisible] = useState(false);
